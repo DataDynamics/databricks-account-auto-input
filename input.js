94059439f190a7de9e3b77266130a33a3e0ec543
HaNeul-Kim <hskimsky@gmail.com>
2022-12-10 14:50:58 +0900

    auto complete 가 가능하도록 변경

---
+++ input.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  document.querySelector('div#login input[name=email]').removeAttribute('autocomplete');
+  // document.querySelector('div#login input[name=email]').setAttribute('autocomplete', 'on');
+}, 500);
